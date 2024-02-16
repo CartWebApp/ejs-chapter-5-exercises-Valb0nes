bf6c4d9639bce8dd4991e7cda8a49c973996725f
Val <143848541+Valb0nes@users.noreply.github.com>
2024-02-16 15:48:27 -0800

    push

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,7 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
+function flatten(array) {
+    return array.reduce((flat, current) => flat.concat(current), []);
+}
 
-
+console.log(flatten([[1,2,3],[4,5],[6]]));
 // → [1, 2, 3, 4, 5, 6]
--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,6 +1,9 @@
-// Your code here.
+function loop(value,test,update,body){
+    while(test(value)){
+        body(value);
+        value = update(value);
+    }
+    return 'loop complete';
+}
 
-loop(3, n => n > 0, n => n - 1, console.log);
-// → 3
-// → 2
-// → 1
+console.log(loop(3, n => n > 0, n => n - 1, console.log));
